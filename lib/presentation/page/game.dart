9f36b62358f4ec0d0a2109609ad27e64fc8d0b4c
fnaoto <f.naoto832+github@gmail.com>
2022-04-21 13:32:50 +0900

    Add player name setting form.

--- lib/presentation/page/game.dart
+++ lib/presentation/page/game.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:sample_flutter_game_with_flame/application/block_app_service.dart';
+import 'package:sample_flutter_game_with_flame/presentation/notifier/player_notifier.dart';
 import 'package:sample_flutter_game_with_flame/presentation/page/result.dart';
 
 class GamePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final _formKey = GlobalKey<FormState>();
+    final playerProvider = ref.watch(playerNotifierProvider);
+    String _playerName = "Player";
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Game'),
@@ -15,14 +20,41 @@ class GamePage extends ConsumerWidget {
           padding: const EdgeInsets.all(30),
           children: <Widget>[
             const Icon(Icons.gamepad, size: 200),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (_) => const ResultPage(),
-                ),
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    initialValue: _playerName,
+                    onChanged: (text) {
+                      _playerName = text;
+                    },
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        throw Exception("Name is empty");
+                      }
+                      return text;
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text('Submit'),
+                    onPressed: () {
+                      if (_playerName.isEmpty) throw Exception("Name is empty");
+                      playerProvider.createPlayer(
+                        name: _playerName,
+                        point: 1,
+                      );
+                      Navigator.of(context).push(
+                        MaterialPageRoute<void>(
+                          builder: (_) => const ResultPage(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
-              child: const Text("go to result"),
-            )
+            ),
           ],
         ),
       ),
