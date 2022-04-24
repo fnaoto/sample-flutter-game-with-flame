753802a0ff901f93198d8bfaaf03b7a22b178d06
fnaoto <f.naoto832+github@gmail.com>
2022-04-24 22:18:23 +0900

    Add flame page.

--- lib/presentation/page/game.dart
+++ lib/presentation/page/game.dart
@@ -1,18 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:sample_flutter_game_with_flame/application/block_app_service.dart';
-import 'package:sample_flutter_game_with_flame/presentation/notifier/player_notifier.dart';
 import 'package:sample_flutter_game_with_flame/presentation/page/drawer.dart';
-import 'package:sample_flutter_game_with_flame/presentation/page/result.dart';
+import 'package:sample_flutter_game_with_flame/presentation/page/flame.dart';
+import 'package:sample_flutter_game_with_flame/presentation/page/home.dart';
 
 class GamePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final _formKey = GlobalKey<FormState>();
-    final playerProvider = ref.watch(playerNotifierProvider);
-    String _playerName = "Player-${DateTime.now().millisecondsSinceEpoch}";
-
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -22,48 +16,18 @@ class GamePage extends ConsumerWidget {
       ),
       drawer: DrawerPage(),
       body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(30),
-          children: <Widget>[
-            const Icon(Icons.gamepad, size: 200),
-            Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  TextFormField(
-                    initialValue: _playerName,
-                    onChanged: (text) {
-                      _playerName = text;
-                    },
-                    validator: (text) {
-                      if (text == null || text.isEmpty) {
-                        throw Exception("Name is empty");
-                      }
-                      return text;
-                    },
-                  ),
-                  ElevatedButton(
-                    child: const Text('Submit'),
-                    onPressed: () {
-                      if (_playerName.isEmpty) throw Exception("Name is empty");
-                      playerProvider.createPlayer(
-                        name: _playerName,
-                        point: Random().nextInt(100),
-                      );
-                      Navigator.of(context).push(
-                        MaterialPageRoute<void>(
-                          builder: (_) => const ResultPage(),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
+        child: GameWidget(
+          game: MyGame(),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute<void>(
+            builder: (_) => const HomePage(),
+          ),
+          (_) => false,
+        ),
+        child: const Text("End"),
       ),
     );
   }
--- lib/presentation/page/result.dart
+++ lib/presentation/page/result.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sample_flutter_game_with_flame/presentation/notifier/player_notifier.dart';
 import 'package:sample_flutter_game_with_flame/presentation/page/drawer.dart';
 import 'package:sample_flutter_game_with_flame/presentation/page/home.dart';
 import 'package:sample_flutter_game_with_flame/presentation/page/players.dart';
@@ -9,6 +12,46 @@ class ResultPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final _formKey = GlobalKey<FormState>();
+    final playerProvider = ref.watch(playerNotifierProvider);
+    String _playerName = "Player-${DateTime.now().millisecondsSinceEpoch}";
+
+    Widget _userForm = Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            initialValue: _playerName,
+            onChanged: (text) {
+              _playerName = text;
+            },
+            validator: (text) {
+              if (text == null || text.isEmpty) {
+                throw Exception("Name is empty");
+              }
+              return text;
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Submit'),
+            onPressed: () {
+              if (_playerName.isEmpty) throw Exception("Name is empty");
+              playerProvider.createPlayer(
+                name: _playerName,
+                point: Random().nextInt(100),
+              );
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (_) => const ResultPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -23,6 +66,7 @@ class ResultPage extends ConsumerWidget {
           padding: const EdgeInsets.all(30),
           children: <Widget>[
             const Icon(Icons.score, size: 200),
+            _userForm,
             ElevatedButton(
               onPressed: () => Navigator.of(context).push(
                 MaterialPageRoute<void>(
